a58b58951b43ee5b2c3b3384667d81bc1697cfdb
move <move@zealand.dk>
2024-02-22 11:41:53 +0100

    Added a little

--- ActorRest2/Controllers/ActorsController.cs
+++ ActorRest2/Controllers/ActorsController.cs
@@ -1,5 +1,6 @@
 ﻿using ActorRepositoryLib;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,10 +18,21 @@ namespace ActorRest2.Controllers
         }
 
         // GET: api/<ActorsController>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [HttpGet]
-        public IEnumerable<Actor> Get()
+        public ActionResult<IEnumerable<Actor>> Get()
         {
-            return _actorsRepository.Get(null,null,null,null);
+            IEnumerable<Actor> actorList =
+                _actorsRepository.Get(null, null, null, null);
+            if (actorList.Any())
+            {
+                return Ok(actorList);
+            }
+            else
+            {
+                return NoContent();
+            }
         }
 
         // GET api/<ActorsController>/5
@@ -31,21 +43,35 @@ namespace ActorRest2.Controllers
         }
 
         // POST api/<ActorsController>
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<Actor> Post([FromBody] Actor newActor)
         {
+            try
+            {
+                Actor createdActor = _actorsRepository.Add(newActor);
+                return Created("/" + createdActor.Id, createdActor);
+            }
+            catch (Exception ex) when (ex is ArgumentNullException ||
+                               ex is ArgumentOutOfRangeException)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // PUT api/<ActorsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public Actor? Put(int id, [FromBody] Actor updatedActor)
         {
+            return _actorsRepository.Update(id, updatedActor);
         }
 
         // DELETE api/<ActorsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public Actor? Delete(int id)
         {
+            return _actorsRepository.Delete(id);
         }
     }
 }
